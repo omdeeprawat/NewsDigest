9f9039717b5d5dce7888cd923b1d47d3e02cf75f
omdeeprawat <omdeepr91@gmail.com>
2026-08-15 15:12:37 +0530

    chore: main application layer to print result of scrapers

---
+++ main.py
@@ -0,0 +1,13 @@
+from app.runner import run_scrapers
+
+def main(hours=24):
+  results = run_scrapers(hours=hours)
+  print(f"youtube videos : {len(results['youtube'])}")
+  print(f"openai articles : {len(results['openai'])}")
+  print(f"anthropic articles : {len(results['anthropic'])}")
+
+  return results
+
+if __name__ == "__main__":
+    print("__main__ BLOCK STARTED")
+    main(hours = 150)
